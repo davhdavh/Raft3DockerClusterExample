0fc11ddde47bde915f43979050fcf87026f7bbe2
Dennis Haney <davh@voxmeter.dk>
2023-02-13 19:17:44 +0700

    Nicer logging, and infrastructure to report latency (not enabled)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using DotNext.Net.Cluster;
 using DotNext.Net.Cluster.Consensus.Raft;
 using DotNext.Net.Cluster.Consensus.Raft.Http;
@@ -11,18 +12,33 @@ builder.Configuration.AddEnvironmentVariables("CLUSTER_");
 
 var services = builder.Services;
 
+var logprefix = builder.Configuration.GetSection("HttpClusterMember").Get<Uri>()?.ToString()?.Split("_").Last()?.Replace('/', ':') ?? throw new MissingFieldException("HttpClusterMember");
+
+builder.Logging.ClearProviders();
+
+builder.Logging.AddSimpleConsole(o =>
+{
+   o.IncludeScopes = false;
+   o.SingleLine = true;
+   o.TimestampFormat = "HH:mm:ss.fff "+logprefix;
+   o.UseUtcTimestamp = true;
+});
+
 void ConfigureRaftCluster()
 {
    builder.JoinCluster();
 
-   var me = builder.Configuration.GetSection("HttpClusterMember").Get<Uri>() ??
-            throw new MissingFieldException("HttpClusterMember");
-   var knownMembers = builder.Configuration.GetSection("HttpClusterMembers").Get<Uri[]>() ??
-                      throw new MissingFieldException("HttpClusterMembers");
+   //services.AddSingleton<MetricsCollector, Metrics>();
+
+   var me = builder.Configuration.GetSection("HttpClusterMember").Get<Uri>() ?? throw new MissingFieldException("HttpClusterMember");
+   var knownMembers = builder.Configuration.GetSection("HttpClusterMembers").Get<Uri[]>() ?? throw new MissingFieldException("HttpClusterMembers");
    services.Configure<HttpClusterMemberConfiguration>(o =>
    {
       o.PublicEndPoint = me;
+      //o.LowerElectionTimeout = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
+      //o.UpperElectionTimeout = (int)TimeSpan.FromSeconds(10).TotalMilliseconds;
       o.Id = ClusterMemberId.FromEndPoint(new UriEndPoint(me));
+      Console.WriteLine($"I am {me} {o.Id}");
       o.ColdStart = knownMembers.Length <= 1;
    });
    services.UseInMemoryConfigurationStorage(endPoints =>
@@ -72,3 +88,44 @@ app.MapRazorPages();
 app.MapControllers();
 
 app.Run();
+
+public class Metrics : HttpMetricsCollector
+{
+   private readonly ILogger<Metrics> _logger;
+
+   public Metrics(ILogger<Metrics> logger)
+   {
+      _logger = logger;
+   }
+
+   public override void ReportResponseTime(TimeSpan value)
+   {
+      _logger.LogInformation($"ResponseTime {value.TotalMilliseconds:F2}ms");
+      base.ReportResponseTime(value);
+   }
+
+   public override void MovedToCandidateState()
+   {
+      base.MovedToCandidateState();
+   }
+
+   public override void MovedToFollowerState()
+   {
+      base.MovedToFollowerState();
+   }
+
+   public override void MovedToLeaderState()
+   {
+      base.MovedToLeaderState();
+   }
+
+   public override void ReportBroadcastTime(TimeSpan value)
+   {
+      base.ReportBroadcastTime(value);
+   }
+
+   public override void ReportHeartbeat()
+   {
+      base.ReportHeartbeat();
+   }
+}
